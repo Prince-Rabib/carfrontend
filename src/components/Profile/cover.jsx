14140fa7ecdb033d16ec99919cf09a372542854f
rabib <rabibhossain123@gmail.com>
2022-08-22 06:31:45 +0600

    message

---
+++ src/components/Profile/cover.jsx
@@ -0,0 +1,431 @@
+import React,{useState,useEffect,useRef, useContext } from 'react'
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import {Card, makeStyles,Grid,Paper, Avatar, TextField, Button, Typography,Link,CardMedia ,CardActions, CardActionArea, CardContent,} from '@material-ui/core'
+import { useHistory,useLocation } from "react-router-dom";
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+import axios from 'axios';
+import VpnKeyIcon from '@material-ui/icons/VpnKey';
+import Select from '@material-ui/core/Select';
+import BuildIcon from '@material-ui/icons/Build';
+import { Phone } from '@material-ui/icons';
+import Rating from '@material-ui/lab/Rating';
+import Box from '@material-ui/core/Box';
+import Tooltip from '@material-ui/core/Tooltip';
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import {host} from '../../host';
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  deleteObject
+} from 'firebase/storage';
+import { storage } from '../../Firebase/firebase';
+import { v4 } from 'uuid';
+
+
+const labels = {
+  0.5: 'Useless',
+  1: 'Useless+',
+  1.5: 'Poor',
+  2: 'Poor+',
+  2.5: 'Ok',
+  3: 'Ok+',
+  3.5: 'Good',
+  4: 'Good+',
+  4.5: 'Excellent',
+  5: 'Excellent+',
+};
+
+const useStyles = makeStyles((theme) =>({
+    container:{
+        
+        marginTop:theme.spacing(2),
+        height:'100%',
+        display:'flex',
+        flexDirection: 'column'
+        
+     },
+      containernew:{
+        
+        marginTop:theme.spacing(2),
+        height:'50%',
+        display:'flex',
+        marginLeft:theme.spacing(1),
+        marginRight:theme.spacing(1),
+        alignItems:"center",
+        justifyContent:"space-between",
+          [theme.breakpoints.down("sm")]:{
+               display:'flex',
+               flexDirection: 'column'
+         }
+        
+     },
+     media:{
+         height:1,
+         paddingTop:'56.25%',
+         
+         [theme.breakpoints.down("sm")]:{
+              height:150
+         }
+     },
+       Media1:{
+         height:400,
+         width:500,
+         paddingTop:'56.25%',
+         [theme.breakpoints.down("sm")]:{
+              height:150
+         }
+     },
+     Button:{
+         marginTop:theme.spacing(2),
+         display:'flex',
+         flexDirection: 'row',
+         justifyContent:'right'
+     },
+     avatar:{
+         display:'flex',
+         
+         
+     },
+     Name:{
+         marginTop:5,
+         marginLeft:5,
+         
+     },
+     Status:{
+         marginTop:theme.spacing(2),
+     },
+      root: {
+        width: 200,
+        height:100,
+        display: 'flex',
+        alignItems: 'center',
+      },
+      inline: {
+        display: 'inline',
+      },
+      Comments:{
+           display:"flex",
+           flexDirection:"column",
+           marginLeft:theme.spacing(2),
+      },
+      dividerColor: {
+       Color: 'blue',
+      },
+      CommentWrite:{
+        
+          display: 'flex',
+          width: '100%',
+          maxWidth: '100vh',
+          backgroundColor: theme.palette.background.paper,
+          
+        
+      },
+
+
+      
+    
+      Container:{
+        
+        marginBottom:theme.spacing(5),
+        marginTop:theme.spacing(2),
+        height:'100%',
+        display:'flex',
+        flexDirection: 'column'
+        
+     },
+     Media:{
+         height:350,
+         paddingTop:'56.25%',
+         [theme.breakpoints.down("sm")]:{
+              height:350
+         }
+     },
+     setup:{
+        marginTop:theme.spacing(5),
+     },
+     numberoflines:{
+          WebkitLineClamp:1
+     },
+     pagination:{
+          marginTop: theme.spacing(5),
+          display: "flex",
+          justifyContent: "center"
+     },
+     root1: {
+        borderRadius:15,
+        background: 'white',
+        marginRight: theme.spacing(2)
+      },
+     
+      textField: {
+        marginLeft: theme.spacing(0),
+        marginRight: theme.spacing(2),
+        width: '43vh',
+        marginTop: theme.spacing(12),
+        justifyContent: "center",
+        [theme.breakpoints.up("sm")]:{
+            marginLeft: theme.spacing(8),
+        }
+        
+        
+      },
+      loading: {
+        
+            width: '100%',
+            justifyContent:'center',
+            '& > * + *': {
+              marginTop: theme.spacing(2),
+            },
+      
+        },
+        ratting:{
+          display:"flex",
+          alignItems:"center"
+
+        },
+          fab: {
+          margin: theme.spacing(2),
+        },
+        absolute: {
+          position: 'absolute',
+          bottom: theme.spacing(2),
+          right: theme.spacing(3),
+        },
+        tooltip:{
+          textAlign:'center'
+         
+        },
+        start:{
+        minWidth: '100%',
+        display: "flex",
+        justifyContent: "flex-end"
+        },
+        begin:{
+          minWidth: '100%',
+          display: "flex",
+          },
+          input:{
+            marginTop: theme.spacing(1)
+          }
+}))
+
+
+
+
+const Cover = () => {
+  const classes = useStyles();
+  const [Details, setDetails] = useState([]);
+  const [trend, setTrend] = useState([]);
+  const [search,setSearch] =useState();
+  const [open, setOpen] = React.useState(false);
+  const [loading,setLoading] =useState(false);
+  const [scroll, setScroll] = React.useState('paper');
+
+
+  const [imageUpload, setImageUpload] = useState('');
+
+
+  const [imageInfo, setImageInfo] = useState([]);
+  const [profile, setProfile] = useState([]);
+
+  const uploadFile = () => {
+    if (imageUpload == null) return;
+    const dynamicImageName = `carReselling/${imageUpload.name + v4()}`;
+    const imageRef = ref(storage, dynamicImageName);
+    uploadBytes(imageRef, imageUpload).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        // dispatch(formActions.addImageInfoFn({url: url, fileName: dynamicImageName}));
+        setImageInfo({url: url, fileName: dynamicImageName});
+        console.log(url);
+      });
+    });
+  };
+
+  const uploadFileProfile = () => {
+    if (imageUpload == null) return;
+    const dynamicImageName = `carReselling/${imageUpload.name + v4()}`;
+    const imageRef = ref(storage, dynamicImageName);
+    uploadBytes(imageRef, imageUpload).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        // dispatch(formActions.addImageInfoFn({url: url, fileName: dynamicImageName}));
+        setProfile({url: url, fileName: dynamicImageName});
+        console.log(url);
+      });
+    });
+  };
+  
+//////////////////////ratting///////////////////////////
+
+  const [value, setValue] = React.useState(2);
+  const [hover, setHover] = React.useState(-1);
+
+///////////////////////////////////////////////////////
+
+
+
+
+
+  let baseUrl;
+
+
+   const changePage = ()=>{
+         
+    
+    window.scroll(0,0);
+}
+  const handleClickOpen = (scrollType) => () => {
+    setOpen(true);
+    setScroll(scrollType);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    
+  };
+
+  const descriptionElementRef = React.useRef(null);
+
+  //////////////////////////////////////////////////////////////
+  
+      const [fieldError,setError] = useState({                
+                 name:"",
+                 phone:"",
+                 Phone:"",
+                 Status:"",
+                 address:"",
+                 mechanic:"",
+                 date:""});
+
+
+
+   const history = useHistory();
+   const {state} = useLocation();
+
+    const handleRoute = (result) =>{      
+    history.push('/'+result)
+   }
+    
+    const avatarStyle={backgroundColor:'#3F51B5'}
+    const btnstyle={margin:'8px 0'}
+
+
+    const exampleInput = useRef();
+    const loginHandler = async(e)=>{
+          
+        console.log("working");
+        
+        try {
+
+        const config = {
+            headers:{
+                "content-Type":"application/json",
+                "x-auth-token": localStorage.getItem("authToken")
+            }
+        }
+
+
+            let data = await axios.post(host+"/api/profiles"+[e],{                
+                "coverimage":imageInfo.url,
+
+              },config);
+
+             handleClose();
+             window.location.reload(false);
+            } catch(error) {
+
+            const err = error.response.data 
+           }
+   }
+
+
+   const Profilehandler = async(e)=>{
+          
+    console.log("working");
+    
+    try {
+
+    const config = {
+        headers:{
+            "content-Type":"application/json",
+            "x-auth-token": localStorage.getItem("authToken")
+        }
+    }
+
+
+        let data = await axios.post(host+"/api/profiles"+[e],{                
+            "profileimage":profile.url,
+
+          },config);
+         console.log(profile.url + "working")
+         handleClose();
+         window.location.reload(false);
+        } catch(error) {
+
+        const err = error.response.data 
+       }
+}
+
+
+
+
+     useEffect(async() => {
+                console.log("Details "+Details);      
+                
+
+      },[search])
+
+   return (
+       <>
+        <div className={classes.start}>
+        <input
+          className={classes.input}
+        type="file"
+        onChange={(event) => {
+        setImageUpload(event.target.files[0]);
+          
+        }}
+        
+      />
+      
+      <Button variant="contained" color="primary" onClick={uploadFile}>
+  Upload Now
+</Button>
+        </div>
+        <div className={classes.begin}>
+        <input
+          className={classes.input}
+        type="file"
+        onChange={(event) => {
+        setImageUpload(event.target.files[0]);
+          
+        }}
+        
+      />
+      
+      <Button variant="contained" color="primary" onClick={uploadFileProfile}>
+  Upload Now
+</Button>
+        </div>
+
+       
+      <div>
+
+      </div>
+
+        {(imageInfo.length === 0)? <h1></h1> : loginHandler()}
+        {(profile.length === 0)? <h1></h1> : Profilehandler()}
+        
+ 
+    </>
+   )
+};
+
+export default Cover;
